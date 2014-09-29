7cc6968bc1a75866a05cec3b8bc0fb9ba5d5ee43
Jay Bhargav Barbhaiya <jaybarb1990@gmail.com>
2014-09-29 20:04:26 +0200

    first commit

---
+++ simple_cms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="simple_cms.MvcApplication" Language="C#" %>
---
+++ simple_cms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(simple_cms.Startup))]
+namespace simple_cms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
